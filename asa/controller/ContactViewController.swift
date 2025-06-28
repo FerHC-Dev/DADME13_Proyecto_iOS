cf89823a07e3c40e3c5e029b9002b6ba3b0c7fc4
Fernando <fernandohcfie@gmail.com>
2025-06-27 18:41:21 -0600

    Correccion de scroll Contacto

--- asa/controller/ContactViewController.swift
+++ asa/controller/ContactViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ContactViewController: UIViewController, UITextViewDelegate {
+class ContactViewController: UIViewController, UITextViewDelegate, UITextFieldDelegate {
     
     @IBOutlet weak var scrollView: UIScrollView!
     
@@ -19,10 +19,22 @@ class ContactViewController: UIViewController, UITextViewDelegate {
         textField.delegate = self
         textField.text = "Dinos tu mensaje"
         textField.textColor = .lightGray
+        
+        // Delegados de los campos
+        setupDelegates(for: scrollView)
+        
+        // Observadores del teclado
+        NotificationCenter.default.addObserver(self,selector: #selector(keyboardWillShow),name: UIResponder.keyboardWillShowNotification,object: nil)
+
+        NotificationCenter.default.addObserver(self,selector: #selector(keyboardWillHide),name: UIResponder.keyboardWillHideNotification,object: nil)
     
+    }
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
     
     func textViewDidBeginEditing(_ textView: UITextView) {
+        scrollToActiveField(textView)
         if textView.textColor == .lightGray {
             textView.textColor = .black
             textView.text = ""
@@ -33,6 +45,46 @@ class ContactViewController: UIViewController, UITextViewDelegate {
         if textView.text.isEmpty {
             textView.textColor = .lightGray
             textView.text = "Dinos tu mensaje"
+        }
+    }
+
+    // MARK: - Observadores del teclado
+    
+    @objc func keyboardWillShow(notification: Notification) {
+        guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+        let keyboardHeight = keyboardFrame.height
+        scrollView.contentInset.bottom = keyboardHeight
+        scrollView.verticalScrollIndicatorInsets.bottom = keyboardHeight
+    }
+
+    @objc func keyboardWillHide(notification: Notification) {
+        scrollView.contentInset.bottom = 0
+        scrollView.verticalScrollIndicatorInsets.bottom = 0
+    }
+        
+    // MARK: - Scroll al campo activo
+
+    func scrollToActiveField(_ activeField: UIView) {
+        let frameInScroll = activeField.convert(activeField.bounds, to: scrollView)
+        scrollView.scrollRectToVisible(frameInScroll, animated: true)
+    }
+        
+    // MARK: - Delegados de campos
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        scrollToActiveField(textField)
+    }
+    
+    func setupDelegates(for view: UIView) {
+        for subview in view.subviews {
+            if let textField = subview as? UITextField {
+                textField.delegate = self
+            } else if let textView = subview as? UITextView {
+                textView.delegate = self
+            } else {
+                // Recursividad para buscar en subviews anidadas
+                setupDelegates(for: subview)
+            }
         }
     }
 
